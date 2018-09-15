9a7c84af9969be699fefd5ac260ea6fe13a6cf78
Moussa Thimbo <moussa.thimbo@gmail.com>
2018-09-15 18:39:31 -0400

    ajout commentaire et correction bug

--- app/Http/Controllers/AyatController.php
+++ app/Http/Controllers/AyatController.php
@@ -8,18 +8,32 @@ class AyatController extends Controller
 {
     protected $data = array();
   
+  /***********************************************************
+	Méthode principale pour retourner tous les ayats du quran.
+  ************************************************************/
   public function index(){
 	  $this->data = $this->getData();
 	  //var_dump($this->data); die();
 	  return response()->json($this->data, 200);
   }
   
-  public function ayats($id){
+  /************************************************************
+  Retourne le ayat numero {id} du quran
+  retourne tous les ayats si le id est null
+  @param id : le numero du ayat dans le quran
+  *************************************************************/
+  public function ayats($id = null){
+	  //TODO: Validation id entre 1 et total data
 	  $this->data = $this->getData();
-	  $ayat = $this->data[$id-1];
+	  $ayat = $id !== null ? $this->data[$id-1] : $this->data;
 	  return response()->json($ayat, 200);
   }
   
+  /**********************************************************************
+  Cette méthode recherche les ayats correspondant à la string recherchée.
+  On enlève d'abord les accents avant de faire la comparaison des string.
+  @param str : le ayat à rechercher
+  ************************************************************************/
   public function search($str){
 	  $result= array();
 	  
@@ -34,13 +48,19 @@ class AyatController extends Controller
 	   }
 	  return $result;
   }
-	  
+	
+  /************************************************************************
+	Récupérer tous les informations des ayats dans le quran.
+  *************************************************************************/
   public function getData(){
 	  $str = file_get_contents(base_path('datajson/quran_details.json'));
 	  $this->data = json_decode($str, true);
 	  return $this->data;
   }
   
+  /************************************************************************
+	Enlève les kharaqates (accents) dans la chaine en arabe.
+  *************************************************************************/
   public function stripAccent($string){
 	$remove = array('ِ', 'ُ', 'ٓ', 'ٰ', 'ْ', 'ٌ', 'ٍ', 'ً', 'ّ', 'َ');
 	$string = str_replace($remove, '', $string);
--- app/Http/Controllers/CoranController.php
+++ app/Http/Controllers/CoranController.php
@@ -13,6 +13,10 @@ class CoranController extends Controller
 	  return response()->json($this->getData(), 200);
 	}
 	
+	/************************************************************
+	  Retourne la sourate du numero envoyé en paramètre.
+	  @param idSourate : le numero de la sourate dans le quran
+    *************************************************************/
 	public function sourate($idSourate){
 		$this->data = $this->getSourate($idSourate);
 		if(!empty($this->data)){
@@ -21,14 +25,21 @@ class CoranController extends Controller
 		
 		
 	}
-	  
+   /************************************************************************
+	Récupérer tous les données des sourates dans le quran.
+  *************************************************************************/
   public function getData(){
 	  $str = file_get_contents(base_path('datajson/surah.json'));
 	  $this->data = json_decode($str, true);
 	  return $this->data;
   }
   
+  /************************************************************
+	  Retourne la sourate du numero envoyé en paramètre.
+	  @param id : le numero de la sourate dans le quran
+    *************************************************************/
   public function getSourate($id){
+	  //TODO validation ID qui doit être entre 1 et 114 sinon BAD_REQUEST
 	   $str = file_get_contents(base_path('datajson/surah/surah_'.$id.'.json'));
 	  
 	  return json_decode($str, true);
